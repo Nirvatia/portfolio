25cdb117733a9cc4a0c761116ff01ad8e66ff7ca
Vi <71043551+Nirvatia@users.noreply.github.com>
2026-08-30 00:48:44 +0800

    fix: vercel deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
+
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,7 +7,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({})
+		adapter: adapter()
 	}
 };
 export default config;
